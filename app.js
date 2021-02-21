ae9a61c6fa73646c33d2a126b70d999b86811058
Nikolas Syveröd <nikolas.syverod@gmail.com>
2021-02-22 01:30:50 +0200

    nothing

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-let scissor = document.getElementById("scissor-button")
-let rock = document.getElementById("rock-button")
-let paper = document.getElementById("paper-button")
+// objects for every button
 
-// function that changes the photo to rock when the "rock" button is pressed
-rock.addEventListener("click", changePicToRock)
+const buttonValues = [
+  { title: "rock", values: ["block", "none", "none"] },
+  { title: "paper", values: ["none", "block", "none"] },
+  { title: "scissor", values: ["none", "none", "block"] },
+];
 
+// loop through every object
+buttonValues.forEach(function(item){
+  //create eventlistener
+  let btn = document.getElementById(item.title + "-button")
+  btn.addEventListener("click", function(e){
+    //depending on the object the images will get different display attribute
+    console.log(item)
+    document.getElementById("rock-user").style.display = item.values[0]
+    document.getElementById("paper-user").style.display = item.values[1]
+    document.getElementById("scissor-user").style.display = item.values[2]
+  })
 
-function changePicToRock(e){
-
-  document.getElementById("paper-user").style.display = "none"
-  document.getElementById("scissor-user").style.display = "none"
-  document.getElementById("rock-user").style.display = "block"
-
-}
-
-
-
-// function that changes the photo to paper when the "paper" button is pressed
-paper.addEventListener("click", changePicToPaper)
-
-function changePicToPaper(e){
-
-  document.getElementById("paper-user").style.display = "block"
-  document.getElementById("scissor-user").style.display = "none"
-  document.getElementById("rock-user").style.display = "none"
-}
-
-
-
-
-// function that changes the photo to scissor when the "scissor" button is pressed
-
-scissor.addEventListener("click", changePicToScissor)
-
-function changePicToScissor(e){
-
-  document.getElementById("paper-user").style.display = "none"
-  document.getElementById("scissor-user").style.display = "block"
-  document.getElementById("rock-user").style.display = "none"
-
-}
+})
