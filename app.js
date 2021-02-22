41754db9a24d3b4ce3fbbe048703868018a9b5fe
Nikolas Syveröd <nikolas.syverod@gmail.com>
2021-02-22 20:26:41 +0200

    worked on the image and text changes

--- app.js
+++ app.js
@@ -16,6 +16,36 @@ buttonValues.forEach(function(item){
     document.getElementById("rock-user").style.display = item.values[0]
     document.getElementById("paper-user").style.display = item.values[1]
     document.getElementById("scissor-user").style.display = item.values[2]
+    document.getElementById("user-answer").textContent = item.title
   })
 
 })
+
+let submitBtn = document.getElementById("submit-btn")
+
+submitBtn.addEventListener("click", runEvent)
+
+function runEvent(e){
+
+  e.preventDefault()
+  let imgs = document.getElementsByClassName("image")
+  let imgs2 = document.getElementsByClassName("image2")
+  let botText = document.getElementById("bot-answer")
+  console.log("submitbtn")
+  let rps = ["rock", "paper", "scissor"]
+  let botAnswer = rps[Math.floor(Math.random() * rps.length)]
+  botText.textContent = botAnswer
+  console.log(botAnswer)
+  
+  //add class to every element
+/*   for(let i = 0; i < imgs; i++){
+
+    imgs[i].classList.add("image-loop")
+    imgs2[i].classList.add("image-loop")
+
+  } */
+
+  
+  
+
+}
